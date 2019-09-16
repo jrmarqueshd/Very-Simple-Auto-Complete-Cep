0c2c8087baf3cf28f2e4063102b6fbaad755fedb
jrmarqueshd <gilmar_junior@hotmail.com.br>
2019-09-15 21:17:19 -0300

    adding new api to request CEP

--- js/main.js
+++ js/main.js
@@ -9,64 +9,50 @@ window.addEventListener('load', ()=>{
         if($inputCEP.value.length == 8){
             let url = `http://viacep.com.br/ws/${$inputCEP.value}/json/`;
             fetch(url)
-                .then(res => {
+            .then(res => {
+                if(res.ok){
                     return res.json();
-                })
-                .then((out) =>{
-                    let end = {
-                        cep: out.cep,
-                        address: out.logradouro,
-                        neighborhood: out.bairro,
-                        city: out.localidade,
-                        state: out.uf
-                    }
-                    $address.value = end.address;
-                    $neighborhood.value = end.neighborhood;
-                    $city.value = end.city;
-                    $state.value = end.state                
-                })
-                .catch(err => {
-                    alert("Erro inexperado, por favor contacte o seu administrador!!!", err);
-                });
-            }else{
-                alert("Digite um CEP válido.");
-            }
-    });
-
-
-
-    let cep = "09973220";
-
-    if(cep.length != 8){
-        alert("Digite um valor válido!");
-    }else{
-        // fetch("http://viacep.com.br/ws/" + cep + "/json/")
-        fetch("https://api.postmon.com.br/v1/cep/" + cep)
-            .then(response =>{
-                if(response.ok){
-                    response.json().then(myJson =>{
-                        console.log(myJson);
+                }else{
+                    fetch("https://api.postmon.com.br/v1/cep/"+$inputCEP.value)
+                    .then((response)=>{
+                        return response.json();
+                    })
+                    .then((out)=>{
+                        let end = {
+                            cep: out.cep,
+                            address: out.logradouro,
+                            neighborhood: out.bairro,
+                            city: out.cidade,
+                            state: out.estado
+                        }
+                        $address.value = end.address;
+                        $neighborhood.value = end.neighborhood;
+                        $city.value = end.city;
+                        $state.value = end.state   
                     })
                     .catch(err =>{
                         console.log(err);
-                    })
-                }else{
-                    return alert("Error!!!");
+                    });
                 }
             })
-            .catch((err)=>{
-                alert(`Houve um erro inexperado (${err}), Entre em contato com a equipe técnica!`);
+            .then((out) =>{
+                let end = {
+                    cep: out.cep,
+                    address: out.logradouro,
+                    neighborhood: out.bairro,
+                    city: out.localidade,
+                    state: out.uf
+                }
+                $address.value = end.address;
+                $neighborhood.value = end.neighborhood;
+                $city.value = end.city;
+                $state.value = end.state                
             })
-    }
-
-    // fetch("https://api.postmon.com.br/v1/cep/" + cep)
-    //     .then((response)=>{
-    //         return response.json();
-    //     })
-    //     .then((myJson)=>{
-    //         console.log(myJson);
-    //     })
-    //     .catch(err =>{
-    //         console.log(err);
-    //     })
+            .catch(err => {
+                alert("Erro inexperado, por favor contacte o seu administrador!!!", err);
+            });
+        }else{
+            alert("Digite um CEP válido.");
+        }
+    });
 });
